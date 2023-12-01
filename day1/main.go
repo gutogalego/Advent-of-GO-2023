f664e6cc737144f5f77b7fa23d3271095a7dc588
Augusto Galego <augustomirandagalego@gmail.com>
2023-12-01 23:38:41 +0100

    .

--- day1/main.go
+++ day1/main.go
@@ -63,6 +63,115 @@ func problem1() {
 
 }
 
+type Stack struct {
+	elements []rune
+	couter   int
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func problem2() {
+
+	digitMap := map[string]int{
+		"zero":  0,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	lines, _ := getLines("input.txt")
+	fmt.Println(lines)
+
+	prefixSet := make(map[string]struct{})
+	for word := range digitMap {
+		for i := 1; i <= len(word); i++ {
+			prefixSet[word[:i]] = struct{}{}
+		}
+	}
+
+	reversedPrefixSet := make(map[string]struct{})
+	for word := range digitMap {
+		reversedWord := reverseString(word)
+		for i := 1; i <= len(reversedWord); i++ {
+			reversedPrefixSet[reversedWord[:i]] = struct{}{}
+		}
+	}
+
+	fmt.Println(prefixSet)
+	fmt.Println(reversedPrefixSet)
+
+	first := -1
+	lastDigit := -1
+	sum := 0
+
+	for _, str := range lines {
+		for start, end := 0, 1; start < len(str); {
+			if unicode.IsDigit(rune(str[start])) {
+				first = int(str[start] - '0')
+				break
+			}
+			if _, exists := prefixSet[str[start:end]]; exists {
+				if _, exists := digitMap[str[start:end]]; exists {
+					first = digitMap[str[start:end]]
+					break
+				}
+				if end < len(str) {
+					end++
+				} else {
+					start++
+				}
+			} else {
+				start++
+				if end <= start {
+					end = start + 1
+				}
+			}
+		}
+
+		str := reverseString(str)
+
+		for start, end := 0, 1; start < len(str); {
+			if unicode.IsDigit(rune(str[start])) {
+				lastDigit = int(str[start] - '0')
+				break
+			}
+			if _, exists := reversedPrefixSet[str[start:end]]; exists {
+				if _, exists := digitMap[reverseString(str[start:end])]; exists {
+					lastDigit = digitMap[reverseString(str[start:end])]
+					break
+				}
+				if end < len(str) {
+					end++
+				} else {
+					start++
+				}
+			} else {
+				start++
+				if end <= start {
+					end = start + 1
+				}
+			}
+		}
+
+		total := first*10 + lastDigit
+		sum += total
+	}
+
+	fmt.Println("Sum: ", sum)
+}
+
 func main() {
-	problem1()
+	problem2()
 }
