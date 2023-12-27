72556d491f3b038f49f386af1151a4c2350866e9
Augusto Galego <augustomirandagalego@gmail.com>
2023-12-27 20:20:44 +0100

    .

--- day3/day3.go
+++ day3/day3.go
@@ -157,6 +157,30 @@ func problem1() {
 	fmt.Println("The value of sum is:", sum)
 }
 
+func removeDuplicates(elements []int) []int {
+	// Map to keep track of unique elements
+	seen := make(map[int]bool)
+
+	// Slice to hold the unique elements
+	unique := []int{}
+
+	for _, value := range elements {
+		if _, ok := seen[value]; !ok {
+			seen[value] = true
+			unique = append(unique, value)
+		}
+	}
+
+	return unique
+}
+
+func reverseRunes(runes []rune) []rune {
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return runes
+}
+
 func problem2() {
 	lines, _ := getLines("input.txt")
 	fmt.Println(lines)
@@ -168,61 +192,86 @@ func problem2() {
 		line := lines[idy]
 
 		idx := 0
-		num_start := -1
-		num_end := -1
+
 		for idx < len(line) {
 
 			ch := line[idx]
-			if unicode.IsNumber(rune(ch)) {
-				if num_start == -1 {
-					num_start = idx
-				}
-				num_end = idx
-			}
 
-			if (!unicode.IsNumber(rune(ch)) || idx+1 == len(line)) && num_start != -1 {
+			if ch == '*' {
+				nums := []int{}
 
-				valid := false
-				for i := num_start; i <= num_end; i++ {
-
-					for _, dir := range directions {
-						dir_y := 0
-						if idy+dir[0] > 0 {
-							dir_y = idy + dir[0]
-							if dir_y >= len(lines) {
-								dir_y = len(lines) - 1
-							}
-						}
-
-						dir_x := 0
-						if i+dir[1] > 0 {
-							dir_x = i + dir[1]
-							if dir_x >= len(line) {
-								dir_x = len(line) - 1
-							}
-						}
-
-						candidate_ch := rune(lines[dir_y][dir_x])
-						if !unicode.IsNumber(candidate_ch) && candidate_ch != '.' {
-							valid = true
+				for _, dir := range directions {
+					dir_y := 0
+					if idy+dir[0] > 0 {
+						dir_y = idy + dir[0]
+						if dir_y >= len(lines) {
+							dir_y = len(lines) - 1
 						}
 					}
 
-				}
-				if valid {
-					substr := line[num_start : num_end+1] // Substring from position 1 to 3
-
-					// Convert substring to number
-					num, _ := strconv.Atoi(substr)
-
-					if num > 700 {
-						fmt.Println(substr)
+					dir_x := 0
+					if idx+dir[1] > 0 {
+						dir_x = idx + dir[1]
+						if dir_x >= len(line) {
+							dir_x = len(line) - 1
+						}
 					}
-					sum += num
+
+					candidate_ch := rune(lines[dir_y][dir_x])
+					if unicode.IsNumber(candidate_ch) {
+						fmt.Printf("%c", candidate_ch)
+						rightNumeric := true
+						leftNumeric := true
+						dir_right := dir_x + 1
+						dir_left := dir_x - 1
+						rightChars := []rune{}
+						leftChars := []rune{}
+
+						for dir_left >= 0 && leftNumeric {
+							left_ch := rune(lines[dir_y][dir_left])
+							dir_left--
+							if unicode.IsNumber(left_ch) {
+								leftChars = append(leftChars, left_ch)
+							} else {
+								leftNumeric = false
+							}
+						}
+
+						for dir_right < len(line) && rightNumeric {
+							right_ch := rune(lines[dir_y][dir_right])
+							dir_right++
+							if unicode.IsNumber(right_ch) {
+								rightChars = append(rightChars, right_ch)
+							} else {
+								rightNumeric = false
+							}
+
+						}
+
+						// fmt.Println("left", leftChars)
+						// fmt.Println("candidate_ch", candidate_ch)
+						// fmt.Println("rightChars", rightChars)
+						leftChars = reverseRunes(leftChars)
+						allElements := append(leftChars, candidate_ch)
+						allElements = append(allElements, rightChars...)
+						resultString := string(allElements)
+
+						resultInt, _ := strconv.Atoi(resultString)
+
+						nums = append(nums, resultInt)
+
+					}
+
+				}
+				nums = removeDuplicates(nums)
+
+				if len(nums) == 2 {
+					fmt.Println("nums", nums)
+					sum += nums[0] * nums[1]
 				}
 
-				num_start = -1
-				num_end = -1
+				// Identify numbers around
+
 			}
 			idx++
 		}
